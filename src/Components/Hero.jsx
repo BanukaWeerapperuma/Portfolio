81cac2297788000b5ec81f5f128afaab6ad7fd5d
BanukaWeerapperuma <banukaweerapperuma@gmail.com>
2026-01-01 22:21:48 +0530

    add framer-motion in hero

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,6 +1,24 @@
 import profilePic from "../assets/Profile.webp";
 import CV from "../assets/banuka_weerapperuma.pdf";
 import { HERO_CONTENT } from "../constants";
+import { motion, stagger } from "framer-motion";
+
+const containerVariants = {
+    hidden : {opacity: 0 , x: -100 },
+    visible : {
+      opacity :1 ,
+      x :0 ,
+      transition : {
+        duration: 0.5,
+        staggerChildren: 0.5,
+      }
+    }
+}
+
+const childVariants = {
+  hidden : { opacity : 0 , x: -100},
+  visible : {opacity : 1 , x:0 , transition : {duration : 0.5 }}
+}
 
 const Hero = () => {
   return (
@@ -12,48 +30,51 @@ const Hero = () => {
 
           {/* Profile Image (TOP on mobile) */}
           <div className="w-full lg:w-1/2 flex justify-center lg:justify-end">
-            <img
+            <motion.img
               src={profilePic}
               alt="profile"
               className="border border-stone-900 rounded-3xl max-w-xs sm:max-w-sm lg:max-w-md"
-            />
+              width={650}
+              height={650}
+              initial = {{x:100 , opacity :0 }}
+              animate = {{x:0 , opacity :1 }}
+              transition={{duration :1 , delay : 1.5 }}/>
           </div>
 
           {/* Text Content */}
-          <div className="w-full lg:w-1/2 flex flex-col items-center lg:items-start text-center lg:text-left">
+          <motion.div 
+            initial = "hidden"
+            animate = "visible"
+            variants={containerVariants}
+          
+          
+           className="w-full lg:w-1/2 flex flex-col items-center lg:items-start text-center lg:text-left">
 
-            <h1 className="
-              text-4xl
-              sm:text-5xl
-              lg:text-7xl
-              xl:text-8xl
-              font-bold
-              tracking-tight
-              max-w-xl
-              lg:max-w-none
-            ">
+            <motion.h2 
+            variants={childVariants}
+            className="text-4xl sm:text-5xl lg:text-7x xl:text-8xl font-bold tracking-tight max-w-xl lg:max-w-none">
               Banuka Weerapperuma
-            </h1>
+            </motion.h2>
 
-            <span className="mt-4 bg-linear-to-r from-stone-300 to-stone-600 bg-clip-text text-2xl sm:text-3xl tracking-tight text-transparent">
+            <motion.span 
+            variants={childVariants}
+            className="mt-4 bg-linear-to-r from-stone-300 to-stone-600 bg-clip-text text-2xl sm:text-3xl tracking-tight text-transparent">
               Full Stack Developer
-            </span>
+            </motion.span>
 
-            <p className="mt-6 max-w-lg text-lg sm:text-xl leading-relaxed tracking-tight text-stone-400">
+            <motion.p 
+            variants={childVariants}
+            className="mt-6 max-w-lg text-lg sm:text-xl leading-relaxed tracking-tight text-stone-400">
               {HERO_CONTENT}
-            </p>
+            </motion.p>
 
-            <a
-              href={CV}
-              target="_blank"
-              rel="noopener noreferrer"
-              download
-              className="mt-8 inline-block rounded-full bg-white px-6 py-3 text-sm font-medium text-stone-800 hover:bg-stone-200 transition"
-            >
+            <motion.a
+            variants={childVariants}
+              href={CV} target="_blank" rel="noopener noreferrer" download className="mt-8 inline-block rounded-full bg-white px-6 py-3 text-sm font-medium text-stone-800 hover:bg-stone-200 transition">
               Download Resume
-            </a>
+            </motion.a>
 
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>

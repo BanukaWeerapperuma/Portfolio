9006663b188cd1c44b0c6d830a832dfa9ac542f6
BanukaWeerapperuma <banukaweerapperuma@gmail.com>
2026-01-02 16:59:32 +0530

    finish  protfolio

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
 import { CONTACT } from '../constants'
+import { motion } from "framer-motion";
 
 const Contact = () => {
   return (
     <div className='border-t border-stone-900 pb-20'>
-        <h2 className="my-10 text-center text-4xl">Get in Touch</h2>
+        <motion.h2 
+            whileInView={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 0, y: -100 }}
+            transition={{ duration: 0.5 }}
+        className="my-10 text-center text-4xl">Get in Touch
+        </motion.h2>
         <div className="text-center tracking-tighter">
-            <p className="my-4">
+            <motion.p 
+                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: -100 }}
+                transition={{ duration: 1 }}
+            className="my-4">
                 {CONTACT.address}
-            </p>
+            </motion.p>
 
-            <p className="my-4">
+            <motion.p 
+                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: -100 }}
+                transition={{ duration: 1 }}
+            className="my-4">
                 {CONTACT.phoneNo}
-            </p>
+            </motion.p>
             <a href={`mailto:${CONTACT.email}?subject=Portfolio%20Inquiry&body=Hello%20Banuka,%0A%0AI%20saw%20your%20portfolio%20and%20would%20like%20to%20contact%20you.`} className='border-b'>
                 {CONTACT.email}
             </a>
